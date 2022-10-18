02eab14b4531e3edcc8639494c098ff9b243b401
Magdalene George <92087652+MGkwawi@users.noreply.github.com>
2022-10-18 12:59:22 +0300

    ex.

---
+++ 9. Exercise Data Types and Variables/8. calculator.js
@@ -0,0 +1,18 @@
+function calculator(num, operator, numTwo) {
+    
+    switch (operator) {
+        case '+': console.log((num + numTwo).toFixed(2));
+            break;
+        case '-': console.log((num - numTwo).toFixed(2));
+            break;
+        case '/': console.log((num / numTwo).toFixed(2));
+            break;
+        case '*': console.log((num * numTwo).toFixed(2));
+            break;
+
+    }
+}
+calculator(5,
+    '+',
+    10
+    )
