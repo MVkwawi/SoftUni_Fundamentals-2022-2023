8682b5a13ec31b5529c51562b1732ab129d8bfd8
Magdalene George <92087652+MGkwawi@users.noreply.github.com>
2022-11-09 12:58:15 +0200

    Update 6. passwordValidator.js

--- 15. Exercise_Functions/6. passwordValidator.js
+++ 15. Exercise_Functions/6. passwordValidator.js
@@ -1,25 +1,38 @@
 function passwordValidator(password) {
+    
+    let digitCount = 0;
+    let passwordLength = password.length;
+    let unrequiredSymbols = false;
 
-    function charNumber (password) {
-        let passwordLength = password.length;
+    for (let i = 0; i < passwordLength; i ++) {
+        let currentSymbol = password[i].charCodeAt();
 
-        if (passwordLength > 5 && passwordLength < 11) {
-            return true;
-        } else {
-            return 'Password should be between 6 and 10 characters';
+        let isDigit = currentSymbol >= 48 && currentSymbol <= 57;
+        let isSmallLetter = currentSymbol >= 97 && currentSymbol <= 122;
+        let isCapitalLetter = currentSymbol >= 65 && currentSymbol <= 90;
+        
+        if (isDigit) {
+            digitCount ++;
+        }
+        
+        if (!isDigit && !isSmallLetter && !isCapitalLetter) {
+            unrequiredSymbols = true;
         }
     }
-    let charCheck = charNumber(password);
 
-    //check if the password is between 6-10 characters
-
-    function lettersDigits(password) {
-        
-
-      // should have at least two digits
-    }  
+    if (passwordLength < 6 || passwordLength > 10) {
+        console.log('Password must be between 6 and 10 characters');
     }
-    //check if the password has only letters and digits
+    if (unrequiredSymbols) {
+        console.log('Password must consist only of letters and digits');
+    }
+    if (digitCount < 2) {
+        console.log('Password must have at least 2 digits');
+    }
+    if (digitCount >= 2 && !unrequiredSymbols && (passwordLength >= 6 && passwordLength <= 10)) {
+        console.log('Password is valid');
+    }
+}
 
     
-passwordValidator('logIn');
+passwordValidator('MyPass123');
