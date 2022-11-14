420a7b5b833fa9c376b9aeb64dc22b5a96597b4f
Magdalene George <92087652+MGkwawi@users.noreply.github.com>
2022-11-14 22:23:33 +0200

    Update 3. firstAndLastKNumbers.js

--- 17. Arrays_Advances/3. firstAndLastKNumbers.js
+++ 17. Arrays_Advances/3. firstAndLastKNumbers.js
@@ -1,3 +1,11 @@
-function firstAndLastKNumbers() {
-    
+function firstAndLastKNumbers(array) {
+    let kNumber = array.shift();
+
+    let firstArray = array.slice(0, kNumber);
+    let secondArray = array.slice(array.length - kNumber);
+
+    console.log(firstArray.join(' '));
+    console.log(secondArray.join(' '));
 }
+firstAndLastKNumbers([2, 7, 8, 9]);
+firstAndLastKNumbers([3,6, 7, 8, 9]);
