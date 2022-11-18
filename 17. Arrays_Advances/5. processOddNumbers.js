4f778ec812a811f75bab1af104a991b36c098d52
Magdalene George <92087652+MGkwawi@users.noreply.github.com>
2022-11-18 12:55:27 +0200

    updt

--- 17. Arrays_Advances/5. processOddNumbers.js
+++ 17. Arrays_Advances/5. processOddNumbers.js
@@ -5,3 +5,12 @@ function processOddNumbers(numbers) {
     console.log(reversedNewArray.join(' '));
 }
 processOddNumbers([10, 15, 20, 25]);
+
+function processOddNumbersTwo(numbers) {
+    let newArray = numbers.filter((_, i) => i % 2 !== 0)
+        .map(num => num * 2)
+        .reverse();
+
+    console.log(newArray.join(' '));
+}
+processOddNumbersTwo([3, 0, 10, 4, 7, 3]);
