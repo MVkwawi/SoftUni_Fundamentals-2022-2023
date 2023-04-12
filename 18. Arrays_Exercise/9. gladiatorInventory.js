3f9a9b028ddf52b6092694dd7ed6d21c6dc9f8e9
Magdalene George <92087652+MVkwawi@users.noreply.github.com>
2023-04-12 21:20:37 +0300

    updt

--- 18. Arrays_Exercise/9. gladiatorInventory.js
+++ 18. Arrays_Exercise/9. gladiatorInventory.js
@@ -1,47 +1,51 @@
 function gladiatorInventory(info) {
-    let inventory = info.shift();
+  let inventory = info.shift().split(" ");
 
-    for (const line of info) {
-        let [command, inventoryToChange] = line.split(' ');
-
-    }
+  for (const el of info) {
+    let [command, inventoryToChange] = el.split(" ");
     switch (command) {
-        case 'Buy': 
-            if (!inventory.includes(inventoryToChange)) {
-                inventory.push(inventoryToChange);
-            }
-            break;
-        case 'Trash': 
-            if (inventory.includes(inventoryToChange)) {
-                let inventoryIndex = inventory.indexOf(inventoryToChange);
-                inventory.splice(inventoryIndex, 1);
-            }
-            break;
-        case 'Repair': 
-            if (inventory.includes(inventoryToChange)) {
-                let inventoryIndex = inventory.indexOf(inventoryToChange);
-                inventory.splice(inventoryIndex, 1);
-                inventory.push(inventoryToChange);
-            }
-            break;
-        case 'Upgrade': 
-            let [newItem, upgr] = inventoryToChange.split('-');
+      case "Buy":
+        if (!inventory.includes(inventoryToChange)) {
+          inventory.push(inventoryToChange);
+        }
+        break;
+      case "Trash":
+        if (inventory.includes(inventoryToChange)) {
+          let inventoryIndex = inventory.indexOf(inventoryToChange);
+          inventory.splice(inventoryIndex, 1);
+        }
+        break;
+      case "Repair":
+        if (inventory.includes(inventoryToChange)) {
+          let inventoryIndex = inventory.indexOf(inventoryToChange);
+          inventory.splice(inventoryIndex, 1);
+          inventory.push(inventoryToChange);
+        }
+        break;
+      case "Upgrade":
+        let [newItem, upgr] = inventoryToChange.split("-");
 
-            if (inventory.includes(newItem)) {
-                inventory[newItem] = newItem + ':' + upgr;
-            }
-            break;
+        if (inventory.includes(newItem)) {
+          let newIndex = inventory.indexOf(newItem) + 1;
+          let upgrdEquipmentInsert = inventoryToChange.split('-').join(':');
+          inventory.splice(newIndex, 0, upgrdEquipmentInsert);
+        }
+        break;
     }
-    console.log(inventory.join(' '));
+  }
+
+  console.log(inventory.join(" "));
 }
-gladiatorInventory(['SWORD Shield Spear',
-'Buy Bag',
-'Trash Shield',
-'Repair Spear',
-'Upgrade SWORD-Steel']
-);
-gladiatorInventory(['SWORD Shield Spear',
-'Trash Bow',
-'Repair Shield',
-'Upgrade Helmet-V']
-);
+gladiatorInventory([
+  "SWORD Shield Spear",
+  "Buy Bag",
+  "Trash Shield",
+  "Repair Spear",
+  "Upgrade SWORD-Steel",
+]);
+gladiatorInventory([
+  "SWORD Shield Spear",
+  "Trash Bow",
+  "Repair Shield",
+  "Upgrade Helmet-V",
+]);
