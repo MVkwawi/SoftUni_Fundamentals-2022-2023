67235e8c1601d8858977e1a329b82f204c4fe841
Magdalene George <92087652+MVkwawi@users.noreply.github.com>
2023-04-15 10:45:45 +0300

    updt

--- 12. Exercise Arrays/8.2. diagonal_attack.js
+++ 12. Exercise Arrays/8.2. diagonal_attack.js
@@ -1,5 +1,48 @@
 function diagonalAttack(matrix) {
-    
+    //parse the strings into nums
+    let newArrayMatrix = [];
+    let matrixLength = matrix.length;
+
+    for (let i = 0; i < matrixLength; i ++) {
+        newArrayMatrix.push(
+            matrix[i].split(' ').map((str) => {
+                return Number(str);
+            })
+        );
+    }
+    //check the diagonal sum
+    let newArrayMatrixLength = newArrayMatrix.length;
+    // let currentDiagonal = 0;
+    // let currentConterDiagonal = newArrayMatrix[0].length - 1;
+    let diagonalSum = 0;
+    let counterDiagonalSum = 0;
+
+    for (let i = 0; i < newArrayMatrixLength; i ++) {
+        diagonalSum += newArrayMatrix[i][i];
+        counterDiagonalSum += newArrayMatrix[i][newArrayMatrixLength - i - 1];
+    }
+    //rewrite the array with the sum = el !== sum
+    let diagonalPosition = 0;
+    let counterDiagonalPosition = newArrayMatrix[0].length - 1;
+
+    if (diagonalSum === counterDiagonalSum) {
+        let rowLength = newArrayMatrix[0].length;
+
+        for (let row = 0; row < rowLength; row ++) {
+            for (let currentColPosition = 0; currentColPosition < rowLength; currentColPosition ++) {
+                
+                if (currentColPosition !== diagonalPosition &&
+                    currentColPosition !== counterDiagonalPosition) {
+                        newArrayMatrix[row][currentColPosition] = diagonalSum;
+                }
+            }
+
+            diagonalPosition ++;
+            counterDiagonalPosition --;
+        }
+    } 
+    //print the arr on separate lines with space      
+        console.log(newArrayMatrix.map((num) => num.join(' ')).join(' \n'));
 }
 
 diagonalAttack(['5 3 12 3 1',
