9ea01bcc4794b7189aa25a95c3886752cb049a22
Magdalene George <92087652+MVkwawi@users.noreply.github.com>
2023-04-15 11:55:59 +0300

    updt

--- 12. Exercise Arrays/9.2. orbit.js
+++ 12. Exercise Arrays/9.2. orbit.js
@@ -1,5 +1,29 @@
-function orbit(cordinates) {
-    
+function orbit(coordinates) {
+    let row = coordinates[0];
+    let col = coordinates[1];
+    let x = coordinates[2];
+    let y = coordinates[3];
+    //print the space
+    let spaceMatrix = [];
+
+    for (let r = 0; r < row; r ++) {
+        spaceMatrix[r] = [];
+
+        for (let c = 0; c < col; c ++) {
+            spaceMatrix[r][c] = 0;
+        }
+    }
+    // put the star and surround it
+    let currentNum = 1;
+
+    for (let r = 0; r < row; r ++) {
+
+        for (let c = 0; c < col; c ++) {
+            spaceMatrix[r][c] = Math.max(Math.abs(r - x), Math.abs(c - y)) + 1;
+        }
+    }
+    //print the orbit " " + \n
+    console.log(spaceMatrix.map((num) => num.join(' ')).join('\n'));
 }
 
 orbit([4, 4, 0, 0]);
