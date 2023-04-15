8daa52954f1e7d2d3e1a691f7cf1daaa96210518
Magdalene George <92087652+MVkwawi@users.noreply.github.com>
2023-04-15 12:34:51 +0300

    updt

--- 18. Arrays_Exercise/1.2. equal_neighbors.js
+++ 18. Arrays_Exercise/1.2. equal_neighbors.js
@@ -1,27 +1,30 @@
-function findNeighborPairs(nums) {
+function findNeighborPairs(info) {
     let pairCounter = 0;
 
-    for (const line of nums) {
-        let elNew = 0;
-        let elOld = 0;
-        let elUp = 0;
-        
-        for (const el of line) {
-            if (elNew === elOld) {
-                pairCounter ++;
-            } 
-        }
+    for (let row = 0; row < info.length; row ++) {
 
-        if (elNew === elUp) {
-            pairCounter ++;
-        }   
+        for (let col = 0; col < info[row].length; col ++) {
+            let currentNum = info[row][col];
+
+            if (currentNum === info[row][col + 1]) {
+                pairCounter ++;
+            }
+
+            if (row + 1 < info.length) {
+                if (currentNum === info[row + 1][col]) {
+                    pairCounter ++;
+                }
+            }
+        }
     }
+
+    console.log(pairCounter);
 }
-// findNeighborPairs([['2', '3', '4', '7', '0'],
-// ['4', '0', '5', '3', '4'],
-// ['2', '3', '5', '4', '2'],
-// ['9', '8', '7', '5', '4']]
-// );
+findNeighborPairs([['2', '3', '4', '7', '0'],
+['4', '0', '5', '3', '4'],
+['2', '3', '5', '4', '2'],
+['9', '8', '7', '5', '4']]
+);
 findNeighborPairs([['test', 'yo', 'yo', 'ho'],
 ['well', 'done', 'no', '6'],
 ['not', 'done', 'yet', '5']]
