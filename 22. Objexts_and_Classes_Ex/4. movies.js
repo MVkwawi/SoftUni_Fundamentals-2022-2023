735c6f6603283885dec7ed0483fe8567e54e0a54
Magdalene George <92087652+MVkwawi@users.noreply.github.com>
2023-11-02 10:43:33 +0200

    updt

--- 22. Objexts_and_Classes_Ex/4. movies.js
+++ 22. Objexts_and_Classes_Ex/4. movies.js
@@ -1,47 +1,45 @@
 function movies(array) {
+  let movies = [];
 
-    let movies = [];
+  array.forEach((el) => {
+    if (el.includes("addMovie")) {
+      let movie = el.replace("addMovie ", "");
+      movies.push({ name: movie });
+    } else if (el.includes("directedBy")) {
+      let [currentMovie, director] = el.split(" directedBy ");
 
-    array.forEach((el) => {
-        if(el.includes('addMovie')) {
-            let movie = el.replace('addMovie ', '');
-            movies.push({ name: movie });
-        } else if (el.includes('directedBy')) {
-            let [currentMovie, director] = el.split(' directedBy ');
-
-
-        movies.forEach((obj) => {
-            if (obj.name === currentMovie) {
-                obj.director = director;
-            }
-        });
-        } else if (el.includes('onDate')) {
-            let [currentMovie, date] = el.split(' onDate ');
-
-            movies.forEach((obj) => {
-                if (obj.name === currentMovie) {
-                    obj.date = date;
-                }
-            });
-        }        
-    });
-
-    movies.forEach((obj) => {
-        if ('name' in obj && 'director' in obj && 'date' in obj) {
-            console.log(JSON.stringify(obj));
+      movies.forEach((obj) => {
+        if (obj.name === currentMovie) {
+          obj.director = director;
         }
-    });
+      });
+    } else if (el.includes("onDate")) {
+      let [currentMovie, date] = el.split(" onDate ");
+
+      movies.forEach((obj) => {
+        if (obj.name === currentMovie) {
+          obj.date = date;
+        }
+      });
+    }
+  });
+
+  movies.forEach((obj) => {
+    if ("name" in obj && "director" in obj && "date" in obj) {
+      console.log(JSON.stringify(obj));
+    }
+  });
 }
 movies([
-    'addMovie Fast and Furious',
-    'addMovie Godfather',
-    'Inception directedBy Christopher Nolan',
-    'Godfather directedBy Francis Ford Coppola',
-    'Godfather onDate 29.07.2018',
-    'Fast and Furious onDate 30.07.2018',
-    'Batman onDate 01.08.2018',
-    'Fast and Furious directedBy Rob Cohen'
-    ]);
+  "addMovie Fast and Furious",
+  "addMovie Godfather",
+  "Inception directedBy Christopher Nolan",
+  "Godfather directedBy Francis Ford Coppola",
+  "Godfather onDate 29.07.2018",
+  "Fast and Furious onDate 30.07.2018",
+  "Batman onDate 01.08.2018",
+  "Fast and Furious directedBy Rob Cohen",
+]);
 
 // movies([
 //     'addMovie The Avengers',
